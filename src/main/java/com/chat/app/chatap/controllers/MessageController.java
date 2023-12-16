89fad2778bde45043e82b08d823be9cc26161a9f
Ayush Patel <ayushpatel4582@gmail.com>
2023-12-16 16:44:46 +0530

    ui + chat box + buttons

--- src/main/java/com/chat/app/chatap/controllers/MessageController.java
+++ src/main/java/com/chat/app/chatap/controllers/MessageController.java
@@ -15,7 +15,7 @@ public class MessageController {
     public Message getMessage(@RequestBody Message message){
         
         try{
-            Thread.sleep(1000);
+            Thread.sleep(500);
         }catch(InterruptedException e){
             e.printStackTrace();
         }
